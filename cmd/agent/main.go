c48cb0d7f5cd7c8f90620c32a0b0b03b937d1917
Ильнур Маматказин <ilnur.mamatkazin@gmail.com>
2022-03-12 21:05:37 +0300

    ПРоверка работоспособности

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,158 +1,239 @@
 package main
 
-import (
-	"bytes"
-	"context"
-	"encoding/binary"
-	"encoding/json"
-	"errors"
-	"fmt"
-	"math/rand"
-	"net/http"
-	"os"
-	"os/signal"
-	"runtime"
-	"strconv"
-	"strings"
-	"sync"
-	"syscall"
-	"time"
+// import (
+// 	"bytes"
+// 	"context"
+// 	"encoding/binary"
+// 	"encoding/json"
+// 	"errors"
+// 	"fmt"
+// 	"math/rand"
+// 	"net/http"
+// 	"os"
+// 	"os/signal"
+// 	"runtime"
+// 	"strconv"
+// 	"strings"
+// 	"sync"
+// 	"syscall"
+// 	"time"
 
-	"github.com/caarlos0/env/v6"
-	"github.com/ilnurmamatkazin/go-devops/cmd/agent/models"
-)
+// 	"github.com/caarlos0/env/v6"
+// 	"github.com/ilnurmamatkazin/go-devops/cmd/agent/models"
+// )
 
-const (
-	ADDRESS        = "127.0.0.1:8080"
-	POLLINTERVAL   = "2s"
-	REPORTINTERVAL = "10s"
-)
+// const (
+// 	ADDRESS        = "127.0.0.1:8080"
+// 	POLLINTERVAL   = "2s"
+// 	REPORTINTERVAL = "10s"
+// )
 
-func main() {
-	var (
-		mutex     sync.Mutex
-		rtm       runtime.MemStats
-		pollCount int64
-		cfg       models.Config
-	)
+// func main() {
+// 	var (
+// 		mutex     sync.Mutex
+// 		rtm       runtime.MemStats
+// 		pollCount int64
+// 		cfg       models.Config
+// 	)
 
-	cfg = models.Config{
-		Address:        ADDRESS,
-		ReportInterval: REPORTINTERVAL,
-		PollInterval:   POLLINTERVAL,
-	}
+// 	cfg = models.Config{
+// 		Address:        ADDRESS,
+// 		ReportInterval: REPORTINTERVAL,
+// 		PollInterval:   POLLINTERVAL,
+// 	}
 
-	if err := env.Parse(&cfg); err != nil {
-		os.Exit(2)
-	}
+// 	if err := env.Parse(&cfg); err != nil {
+// 		os.Exit(2)
+// 	}
 
-	// конструируем HTTP-клиент
-	client := &http.Client{}
-	client.Timeout = time.Second * 2
+// 	// конструируем HTTP-клиент
+// 	client := &http.Client{}
+// 	client.Timeout = time.Second * 2
 
-	client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
-		if len(via) >= 2 {
-			os.Exit(3)
-		}
-		return nil
-	}
+// 	client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+// 		if len(via) >= 2 {
+// 			os.Exit(3)
+// 		}
+// 		return nil
+// 	}
 
-	transport := &http.Transport{}
-	transport.MaxIdleConns = 20
-	client.Transport = transport
+// 	transport := &http.Transport{}
+// 	transport.MaxIdleConns = 20
+// 	client.Transport = transport
 
-	ctx := context.Background()
+// 	ctx := context.Background()
 
-	pi, _ := strconv.Atoi(strings.Split(cfg.PollInterval, "s")[0])
-	ri, _ := strconv.Atoi(strings.Split(cfg.ReportInterval, "s")[0])
+// 	pi, _ := strconv.Atoi(strings.Split(cfg.PollInterval, "s")[0])
+// 	ri, _ := strconv.Atoi(strings.Split(cfg.ReportInterval, "s")[0])
 
-	tickerPoll := time.NewTicker(time.Duration(pi) * time.Second)
-	tickerReport := time.NewTicker(time.Duration(ri) * time.Second)
+// 	tickerPoll := time.NewTicker(time.Duration(pi) * time.Second)
+// 	tickerReport := time.NewTicker(time.Duration(ri) * time.Second)
 
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+// 	quit := make(chan os.Signal, 1)
+// 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
 
-	done := make(chan bool, 1)
+// 	done := make(chan bool, 1)
 
-	go func() {
-	loop:
-		for {
-			select {
-			case <-quit:
-				// fmt.Println("Shutdown Agent ...")
-				// t := time.NewTicker(5 * time.Second)
-				// <-t.C
-				done <- true
+// 	go func() {
+// 	loop:
+// 		for {
+// 			select {
+// 			case <-quit:
+// 				// fmt.Println("Shutdown Agent ...")
+// 				// t := time.NewTicker(5 * time.Second)
+// 				// <-t.C
+// 				done <- true
 
-				break loop
+// 				break loop
 
-			case <-tickerPoll.C:
-				mutex.Lock()
+// 			case <-tickerPoll.C:
+// 				mutex.Lock()
 
-				runtime.ReadMemStats(&rtm)
-				pollCount++
+// 				runtime.ReadMemStats(&rtm)
+// 				pollCount++
 
-				mutex.Unlock()
-			case <-tickerReport.C:
-				mutex.Lock()
+// 				mutex.Unlock()
+// 			case <-tickerReport.C:
+// 				mutex.Lock()
 
-				sendMetric(ctx, client, "gauge", "Alloc", rtm.Alloc, cfg)
-				sendMetric(ctx, client, "gauge", "BuckHashSys", rtm.BuckHashSys, cfg)
-				sendMetric(ctx, client, "gauge", "Frees", rtm.Frees, cfg)
-				sendMetric(ctx, client, "gauge", "GCCPUFraction", rtm.GCCPUFraction, cfg)
-				sendMetric(ctx, client, "gauge", "GCSys", rtm.GCSys, cfg)
-				sendMetric(ctx, client, "gauge", "HeapAlloc", rtm.HeapAlloc, cfg)
-				sendMetric(ctx, client, "gauge", "HeapIdle", rtm.HeapIdle, cfg)
-				sendMetric(ctx, client, "gauge", "HeapInuse", rtm.HeapInuse, cfg)
-				sendMetric(ctx, client, "gauge", "HeapObjects", rtm.HeapObjects, cfg)
-				sendMetric(ctx, client, "gauge", "HeapReleased", rtm.HeapReleased, cfg)
-				sendMetric(ctx, client, "gauge", "HeapSys", rtm.HeapSys, cfg)
-				sendMetric(ctx, client, "gauge", "LastGC", rtm.LastGC, cfg)
-				sendMetric(ctx, client, "gauge", "Lookups", rtm.Lookups, cfg)
-				sendMetric(ctx, client, "gauge", "MCacheInuse", rtm.MCacheInuse, cfg)
-				sendMetric(ctx, client, "gauge", "MCacheSys", rtm.MCacheSys, cfg)
-				sendMetric(ctx, client, "gauge", "MSpanInuse", rtm.MSpanInuse, cfg)
-				sendMetric(ctx, client, "gauge", "MSpanSys", rtm.MSpanSys, cfg)
-				sendMetric(ctx, client, "gauge", "Mallocs", rtm.Mallocs, cfg)
-				sendMetric(ctx, client, "gauge", "NextGC", rtm.NextGC, cfg)
-				sendMetric(ctx, client, "gauge", "NumForcedGC", rtm.NumForcedGC, cfg)
-				sendMetric(ctx, client, "gauge", "NumGC", rtm.NumGC, cfg)
-				sendMetric(ctx, client, "gauge", "OtherSys", rtm.OtherSys, cfg)
-				sendMetric(ctx, client, "gauge", "PauseTotalNs", rtm.PauseTotalNs, cfg)
-				sendMetric(ctx, client, "gauge", "TotalAlloc", rtm.TotalAlloc, cfg)
-				sendMetric(ctx, client, "gauge", "StackInuse", rtm.StackInuse, cfg)
-				sendMetric(ctx, client, "gauge", "StackSys", rtm.StackSys, cfg)
-				sendMetric(ctx, client, "gauge", "Sys", rtm.Sys, cfg)
-				sendMetric(ctx, client, "counter", "PollCount", pollCount, cfg)
-				sendMetric(ctx, client, "gauge", "RandomValue", rand.Float64(), cfg)
+// 				sendMetric(ctx, client, "gauge", "Alloc", rtm.Alloc, cfg)
+// 				sendMetric(ctx, client, "gauge", "BuckHashSys", rtm.BuckHashSys, cfg)
+// 				sendMetric(ctx, client, "gauge", "Frees", rtm.Frees, cfg)
+// 				sendMetric(ctx, client, "gauge", "GCCPUFraction", rtm.GCCPUFraction, cfg)
+// 				sendMetric(ctx, client, "gauge", "GCSys", rtm.GCSys, cfg)
+// 				sendMetric(ctx, client, "gauge", "HeapAlloc", rtm.HeapAlloc, cfg)
+// 				sendMetric(ctx, client, "gauge", "HeapIdle", rtm.HeapIdle, cfg)
+// 				sendMetric(ctx, client, "gauge", "HeapInuse", rtm.HeapInuse, cfg)
+// 				sendMetric(ctx, client, "gauge", "HeapObjects", rtm.HeapObjects, cfg)
+// 				sendMetric(ctx, client, "gauge", "HeapReleased", rtm.HeapReleased, cfg)
+// 				sendMetric(ctx, client, "gauge", "HeapSys", rtm.HeapSys, cfg)
+// 				sendMetric(ctx, client, "gauge", "LastGC", rtm.LastGC, cfg)
+// 				sendMetric(ctx, client, "gauge", "Lookups", rtm.Lookups, cfg)
+// 				sendMetric(ctx, client, "gauge", "MCacheInuse", rtm.MCacheInuse, cfg)
+// 				sendMetric(ctx, client, "gauge", "MCacheSys", rtm.MCacheSys, cfg)
+// 				sendMetric(ctx, client, "gauge", "MSpanInuse", rtm.MSpanInuse, cfg)
+// 				sendMetric(ctx, client, "gauge", "MSpanSys", rtm.MSpanSys, cfg)
+// 				sendMetric(ctx, client, "gauge", "Mallocs", rtm.Mallocs, cfg)
+// 				sendMetric(ctx, client, "gauge", "NextGC", rtm.NextGC, cfg)
+// 				sendMetric(ctx, client, "gauge", "NumForcedGC", rtm.NumForcedGC, cfg)
+// 				sendMetric(ctx, client, "gauge", "NumGC", rtm.NumGC, cfg)
+// 				sendMetric(ctx, client, "gauge", "OtherSys", rtm.OtherSys, cfg)
+// 				sendMetric(ctx, client, "gauge", "PauseTotalNs", rtm.PauseTotalNs, cfg)
+// 				sendMetric(ctx, client, "gauge", "TotalAlloc", rtm.TotalAlloc, cfg)
+// 				sendMetric(ctx, client, "gauge", "StackInuse", rtm.StackInuse, cfg)
+// 				sendMetric(ctx, client, "gauge", "StackSys", rtm.StackSys, cfg)
+// 				sendMetric(ctx, client, "gauge", "Sys", rtm.Sys, cfg)
+// 				sendMetric(ctx, client, "counter", "PollCount", pollCount, cfg)
+// 				sendMetric(ctx, client, "gauge", "RandomValue", rand.Float64(), cfg)
 
-				mutex.Unlock()
-			}
+// 				mutex.Unlock()
+// 			}
 
-		}
-	}()
+// 		}
+// 	}()
 
-	<-done
+// 	<-done
 
-	// fmt.Println("Agent exiting")
+// 	// fmt.Println("Agent exiting")
 
-}
+// }
 
-func sendMetric(ctxBase context.Context, client *http.Client, typeMetric, nameMetric string, value interface{}, cfg models.Config) (err error) {
-	ctx, cancel := context.WithTimeout(ctxBase, 1*time.Second)
-	defer cancel()
+// func sendMetric(ctxBase context.Context, client *http.Client, typeMetric, nameMetric string, value interface{}, cfg models.Config) (err error) {
+// 	ctx, cancel := context.WithTimeout(ctxBase, 1*time.Second)
+// 	defer cancel()
 
-	// err = sendMetricText(ctx, client, typeMetric, nameMetric, value)
-	err = sendMetricJSON(ctx, client, typeMetric, nameMetric, value, cfg)
+// 	// err = sendMetricText(ctx, client, typeMetric, nameMetric, value)
+// 	err = sendMetricJSON(ctx, client, typeMetric, nameMetric, value, cfg)
 
-	return
-}
+// 	return
+// }
 
-// func sendMetricText(ctx context.Context, client *http.Client, typeMetric, nameMetric string, value interface{}) (err error) {
-// 	endpoint := fmt.Sprintf("http://%s/update/%s/%s/%v", cfg., port, typeMetric, nameMetric, value)
+// // func sendMetricText(ctx context.Context, client *http.Client, typeMetric, nameMetric string, value interface{}) (err error) {
+// // 	endpoint := fmt.Sprintf("http://%s/update/%s/%s/%v", cfg., port, typeMetric, nameMetric, value)
+
+// // 	buf := new(bytes.Buffer)
+// // 	err = binary.Write(buf, binary.LittleEndian, value)
+
+// // 	request, err := http.NewRequestWithContext(ctx, http.MethodPost, endpoint, buf)
+// // 	if err != nil {
+// // 		//fmt.Println(err)
+// // 		return
+// // 	}
+
+// // 	request.Header.Set("Content-Type", "text/plain; charset=utf-8")
+
+// // 	// отправляем запрос и получаем ответ
+// // 	response, err := client.Do(request)
+// // 	if err != nil {
+// // 		//fmt.Println(err)
+// // 		return
+// // 	}
+// // 	// печатаем код ответа
+// // 	fmt.Println("Статус-код ", response.Status)
+// // 	defer response.Body.Close()
+// // 	// читаем поток из тела ответа
+// // 	body, err := ioutil.ReadAll(response.Body)
+// // 	if err != nil {
+// // 		fmt.Println(err)
+// // 		return
+// // 	}
+// // 	// и печатаем его
+// // 	fmt.Println(string(body))
+
+// // 	return
+// // }
+
+// func sendMetricJSON(ctx context.Context, client *http.Client, typeMetric, nameMetric string, value interface{}, cfg models.Config) (err error) {
+// 	var metric models.Metric
+
+// 	endpoint := fmt.Sprintf("http://%s/update", cfg.Address)
+
+// 	metric.ID = nameMetric
+// 	metric.MType = typeMetric
+
+// 	switch typeMetric {
+// 	case "counter":
+// 		var i int64
+// 		i = value.(int64)
+// 		metric.Delta = &i
+// 	case "gauge":
+// 		var f float64
+
+// 		switch value := value.(type) {
+// 		case float64:
+// 			f = value
+// 		case uint64:
+// 			f = float64(value)
+// 		case uint32:
+// 			f = float64(value)
+
+// 		default:
+// 		}
+
+// 		if (nameMetric == "GCCPUFraction") && (f == 0) {
+// 			f = rand.Float64()
+// 		} else if (nameMetric == "LastGC") && (f == 0) ||
+// 			(nameMetric == "Lookups") && (f == 0) ||
+// 			(nameMetric == "NumForcedGC") && (f == 0) ||
+// 			(nameMetric == "NumGC") && (f == 0) ||
+// 			(nameMetric == "PauseTotalNs") && (f == 0) {
+// 			f = float64(rand.Intn(100))
+// 		}
+
+// 		metric.Value = &f
+// 	default:
+// 		err = errors.New("недопустимый тип")
+// 		return
+// 	}
+
+// 	b, err := json.Marshal(metric)
+// 	if err != nil {
+// 		//fmt.Println(err)
+// 		return
+// 	}
 
 // 	buf := new(bytes.Buffer)
-// 	err = binary.Write(buf, binary.LittleEndian, value)
+// 	err = binary.Write(buf, binary.LittleEndian, b)
 
 // 	request, err := http.NewRequestWithContext(ctx, http.MethodPost, endpoint, buf)
 // 	if err != nil {
@@ -160,7 +241,8 @@ func sendMetric(ctxBase context.Context, client *http.Client, typeMetric, nameMe
 // 		return
 // 	}
 
-// 	request.Header.Set("Content-Type", "text/plain; charset=utf-8")
+// 	// в заголовках запроса сообщаем, что данные кодированы стандартной URL-схемой
+// 	request.Header.Set("Content-Type", "application/json")
 
 // 	// отправляем запрос и получаем ответ
 // 	response, err := client.Do(request)
@@ -169,98 +251,16 @@ func sendMetric(ctxBase context.Context, client *http.Client, typeMetric, nameMe
 // 		return
 // 	}
 // 	// печатаем код ответа
-// 	fmt.Println("Статус-код ", response.Status)
+// 	// fmt.Println("Статус-код ", response.Status)
 // 	defer response.Body.Close()
 // 	// читаем поток из тела ответа
-// 	body, err := ioutil.ReadAll(response.Body)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 		return
-// 	}
-// 	// и печатаем его
-// 	fmt.Println(string(body))
+// 	// body, err := ioutil.ReadAll(response.Body)
+// 	// if err != nil {
+// 	// 	fmt.Println(err)
+// 	// 	return
+// 	// }
+// 	// // и печатаем его
+// 	// fmt.Println(string(body))
 
 // 	return
 // }
-
-func sendMetricJSON(ctx context.Context, client *http.Client, typeMetric, nameMetric string, value interface{}, cfg models.Config) (err error) {
-	var metric models.Metric
-
-	endpoint := fmt.Sprintf("http://%s/update", cfg.Address)
-
-	metric.ID = nameMetric
-	metric.MType = typeMetric
-
-	switch typeMetric {
-	case "counter":
-		var i int64
-		i = value.(int64)
-		metric.Delta = &i
-	case "gauge":
-		var f float64
-
-		switch value := value.(type) {
-		case float64:
-			f = value
-		case uint64:
-			f = float64(value)
-		case uint32:
-			f = float64(value)
-
-		default:
-		}
-
-		if (nameMetric == "GCCPUFraction") && (f == 0) {
-			f = rand.Float64()
-		} else if (nameMetric == "LastGC") && (f == 0) ||
-			(nameMetric == "Lookups") && (f == 0) ||
-			(nameMetric == "NumForcedGC") && (f == 0) ||
-			(nameMetric == "NumGC") && (f == 0) ||
-			(nameMetric == "PauseTotalNs") && (f == 0) {
-			f = float64(rand.Intn(100))
-		}
-
-		metric.Value = &f
-	default:
-		err = errors.New("недопустимый тип")
-		return
-	}
-
-	b, err := json.Marshal(metric)
-	if err != nil {
-		//fmt.Println(err)
-		return
-	}
-
-	buf := new(bytes.Buffer)
-	err = binary.Write(buf, binary.LittleEndian, b)
-
-	request, err := http.NewRequestWithContext(ctx, http.MethodPost, endpoint, buf)
-	if err != nil {
-		//fmt.Println(err)
-		return
-	}
-
-	// в заголовках запроса сообщаем, что данные кодированы стандартной URL-схемой
-	request.Header.Set("Content-Type", "application/json")
-
-	// отправляем запрос и получаем ответ
-	response, err := client.Do(request)
-	if err != nil {
-		//fmt.Println(err)
-		return
-	}
-	// печатаем код ответа
-	// fmt.Println("Статус-код ", response.Status)
-	defer response.Body.Close()
-	// читаем поток из тела ответа
-	// body, err := ioutil.ReadAll(response.Body)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return
-	// }
-	// // и печатаем его
-	// fmt.Println(string(body))
-
-	return
-}
--- cmd/agent/main_test.go
+++ cmd/agent/main_test.go
@@ -1,45 +1,45 @@
 package main
 
-import (
-	"context"
-	"net/http"
-	"os"
-	"testing"
+// import (
+// 	"context"
+// 	"net/http"
+// 	"os"
+// 	"testing"
 
-	"github.com/caarlos0/env/v6"
-	"github.com/ilnurmamatkazin/go-devops/cmd/agent/models"
-)
+// 	"github.com/caarlos0/env/v6"
+// 	"github.com/ilnurmamatkazin/go-devops/cmd/agent/models"
+// )
 
-func Test_sendMetric(t *testing.T) {
-	type args struct {
-		ctxBase    context.Context
-		client     *http.Client
-		typeMetric string
-		nameMetric string
-		value      interface{}
-	}
-	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
+// func Test_sendMetric(t *testing.T) {
+// 	type args struct {
+// 		ctxBase    context.Context
+// 		client     *http.Client
+// 		typeMetric string
+// 		nameMetric string
+// 		value      interface{}
+// 	}
+// 	tests := []struct {
+// 		name    string
+// 		args    args
+// 		wantErr bool
+// 	}{
+// 		// TODO: Add test cases.
+// 	}
 
-	cfg := models.Config{
-		Address:        ADDRESS,
-		ReportInterval: REPORTINTERVAL,
-		PollInterval:   POLLINTERVAL,
-	}
+// 	cfg := models.Config{
+// 		Address:        ADDRESS,
+// 		ReportInterval: REPORTINTERVAL,
+// 		PollInterval:   POLLINTERVAL,
+// 	}
 
-	if err := env.Parse(&cfg); err != nil {
-		os.Exit(2)
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if err := sendMetric(tt.args.ctxBase, tt.args.client, tt.args.typeMetric, tt.args.nameMetric, tt.args.value, cfg); (err != nil) != tt.wantErr {
-				t.Errorf("sendMetric() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
-}
+// 	if err := env.Parse(&cfg); err != nil {
+// 		os.Exit(2)
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			if err := sendMetric(tt.args.ctxBase, tt.args.client, tt.args.typeMetric, tt.args.nameMetric, tt.args.value, cfg); (err != nil) != tt.wantErr {
+// 				t.Errorf("sendMetric() error = %v, wantErr %v", err, tt.wantErr)
+// 			}
+// 		})
+// 	}
+// }
