34fc19ff3fe1c7643052a92ed24595b4dce195d5
Ильнур Маматказин <ilnur.mamatkazin@gmail.com>
2022-03-22 19:47:04 +0300

    Вернул тесты

--- cmd/server/handlers/handler_test.go
+++ cmd/server/handlers/handler_test.go
@@ -1,67 +1,67 @@
 package handlers
 
-// import (
-// 	"io/ioutil"
-// 	"log"
-// 	"net/http"
-// 	"net/http/httptest"
-// 	"os"
-// 	"testing"
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
 
-// 	"github.com/caarlos0/env/v6"
-// 	"github.com/ilnurmamatkazin/go-devops/cmd/server/models"
-// 	"github.com/ilnurmamatkazin/go-devops/cmd/server/service"
-// 	"github.com/stretchr/testify/assert"
-// 	"github.com/stretchr/testify/require"
-// )
+	"github.com/caarlos0/env/v6"
+	"github.com/ilnurmamatkazin/go-devops/cmd/server/models"
+	"github.com/ilnurmamatkazin/go-devops/cmd/server/service"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
 
-// func testRequest(t *testing.T, ts *httptest.Server, method, path string) (*http.Response, string) {
-// 	req, err := http.NewRequest(method, ts.URL+path, nil)
-// 	require.NoError(t, err)
+func testRequest(t *testing.T, ts *httptest.Server, method, path string) (*http.Response, string) {
+	req, err := http.NewRequest(method, ts.URL+path, nil)
+	require.NoError(t, err)
 
-// 	resp, err := http.DefaultClient.Do(req)
-// 	require.NoError(t, err)
+	resp, err := http.DefaultClient.Do(req)
+	require.NoError(t, err)
 
-// 	respBody, err := ioutil.ReadAll(resp.Body)
-// 	require.NoError(t, err)
+	respBody, err := ioutil.ReadAll(resp.Body)
+	require.NoError(t, err)
 
-// 	defer resp.Body.Close()
+	defer resp.Body.Close()
 
-// 	return resp, string(respBody)
-// }
+	return resp, string(respBody)
+}
 
-// func TestNewRouter(t *testing.T) {
-// 	cfg := models.Config{
-// 		Address:       models.Address,
-// 		StoreInterval: models.StoreInterval,
-// 		StoreFile:     models.StoreFile,
-// 		Restore:       models.Restore,
-// 		Key:           models.Key,
-// 		Database:      models.Database,
-// 	}
+func TestNewRouter(t *testing.T) {
+	cfg := models.Config{
+		Address:       models.Address,
+		StoreInterval: models.StoreInterval,
+		StoreFile:     models.StoreFile,
+		Restore:       models.Restore,
+		Key:           models.Key,
+		Database:      models.Database,
+	}
 
-// 	if err := env.Parse(&cfg); err != nil {
-// 		log.Println("Ошибка чтения конфигурации")
-// 		os.Exit(2)
-// 	}
+	if err := env.Parse(&cfg); err != nil {
+		log.Println("Ошибка чтения конфигурации")
+		os.Exit(2)
+	}
 
-// 	service := service.NewService(&cfg, nil)
-// 	hendler := NewHandler(service)
-// 	router := hendler.NewRouter()
+	service := service.NewService(&cfg, nil)
+	hendler := NewHandler(service)
+	router := hendler.NewRouter()
 
-// 	ts := httptest.NewServer(router)
-// 	defer ts.Close()
+	ts := httptest.NewServer(router)
+	defer ts.Close()
 
-// 	resp, _ := testRequest(t, ts, "POST", "/update/counter/testCounter/100")
-// 	assert.Equal(t, http.StatusOK, resp.StatusCode)
-// 	defer resp.Body.Close()
+	resp, _ := testRequest(t, ts, "POST", "/update/counter/testCounter/100")
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	defer resp.Body.Close()
 
-// 	resp, _ = testRequest(t, ts, "POST", "/update/counter/testCounter/invalid_value")
-// 	assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
-// 	defer resp.Body.Close()
+	resp, _ = testRequest(t, ts, "POST", "/update/counter/testCounter/invalid_value")
+	assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
+	defer resp.Body.Close()
 
-// 	resp, _ = testRequest(t, ts, "POST", "/update/unknown/testCounter/100")
-// 	assert.Equal(t, http.StatusNotImplemented, resp.StatusCode)
-// 	defer resp.Body.Close()
+	resp, _ = testRequest(t, ts, "POST", "/update/unknown/testCounter/100")
+	assert.Equal(t, http.StatusNotImplemented, resp.StatusCode)
+	defer resp.Body.Close()
 
-// }
+}
--- cmd/server/service/service_test.go
+++ cmd/server/service/service_test.go
@@ -1,132 +1,132 @@
 package service
 
-// import (
-// 	"crypto/hmac"
-// 	"encoding/hex"
-// 	"testing"
+import (
+	"crypto/hmac"
+	"encoding/hex"
+	"testing"
 
-// 	"github.com/ilnurmamatkazin/go-devops/cmd/server/models"
-// 	"github.com/ilnurmamatkazin/go-devops/cmd/server/storage"
-// 	"github.com/stretchr/testify/assert"
-// )
+	"github.com/ilnurmamatkazin/go-devops/cmd/server/models"
+	"github.com/ilnurmamatkazin/go-devops/cmd/server/storage"
+	"github.com/stretchr/testify/assert"
+)
 
-// func TestService_SetMetric(t *testing.T) {
-// 	type metric struct {
-// 		ID         string
-// 		MetricType string
-// 		Hash       string
-// 		Delta      int64
-// 		Value      float64
-// 	}
-// 	tests := []struct {
-// 		name    string
-// 		metric  metric
-// 		wantErr bool
-// 	}{
-// 		{
-// 			name:    "pisitive gauge test",
-// 			metric:  metric{ID: "TotalAlloc", MetricType: "gauge", Value: 175368, Hash: "ecd17becc5c0cb83489bbd3387aa351ea590ce64b44ac4b06645f058f36d20c2"},
-// 			wantErr: false,
-// 		},
-// 		{
-// 			name:    "negative gauge test",
-// 			metric:  metric{ID: "TotalAlloc", MetricType: "gauge", Value: 175368},
-// 			wantErr: true,
-// 		},
-// 		{
-// 			name:    "pisitive counter test",
-// 			metric:  metric{ID: "PollCount", MetricType: "counter", Delta: 5, Hash: "b54c435bba4ef9334d8b0ca2938c912a75660e5a152a0fb68a4177dfccdaf9e9"},
-// 			wantErr: false,
-// 		},
-// 		{
-// 			name:    "negative counter test",
-// 			metric:  metric{ID: "PollCount", MetricType: "counter", Delta: 5},
-// 			wantErr: true,
-// 		},
-// 	}
+func TestService_SetMetric(t *testing.T) {
+	type metric struct {
+		ID         string
+		MetricType string
+		Hash       string
+		Delta      int64
+		Value      float64
+	}
+	tests := []struct {
+		name    string
+		metric  metric
+		wantErr bool
+	}{
+		{
+			name:    "pisitive gauge test",
+			metric:  metric{ID: "TotalAlloc", MetricType: "gauge", Value: 175368, Hash: "ecd17becc5c0cb83489bbd3387aa351ea590ce64b44ac4b06645f058f36d20c2"},
+			wantErr: false,
+		},
+		{
+			name:    "negative gauge test",
+			metric:  metric{ID: "TotalAlloc", MetricType: "gauge", Value: 175368},
+			wantErr: true,
+		},
+		{
+			name:    "pisitive counter test",
+			metric:  metric{ID: "PollCount", MetricType: "counter", Delta: 5, Hash: "b54c435bba4ef9334d8b0ca2938c912a75660e5a152a0fb68a4177dfccdaf9e9"},
+			wantErr: false,
+		},
+		{
+			name:    "negative counter test",
+			metric:  metric{ID: "PollCount", MetricType: "counter", Delta: 5},
+			wantErr: true,
+		},
+	}
 
-// 	cfg := models.Config{Key: "qwerty"}
-// 	r := storage.NewStorage(&cfg, nil)
-// 	s := *NewService(&cfg, r)
+	cfg := models.Config{Key: "qwerty"}
+	r := storage.NewStorage(&cfg, nil)
+	s := *NewService(&cfg, r)
 
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
 
-// 			newMetric := models.Metric{
-// 				ID:         tt.metric.ID,
-// 				MetricType: tt.metric.MetricType,
-// 				Hash:       &tt.metric.Hash,
-// 			}
+			newMetric := models.Metric{
+				ID:         tt.metric.ID,
+				MetricType: tt.metric.MetricType,
+				Hash:       &tt.metric.Hash,
+			}
 
-// 			if newMetric.MetricType == "counter" {
-// 				newMetric.Delta = &tt.metric.Delta
-// 			} else {
-// 				newMetric.Value = &tt.metric.Value
-// 			}
+			if newMetric.MetricType == "counter" {
+				newMetric.Delta = &tt.metric.Delta
+			} else {
+				newMetric.Value = &tt.metric.Value
+			}
 
-// 			err := s.SetMetric(newMetric)
+			err := s.SetMetric(newMetric)
 
-// 			assert.Equal(t, (err != nil), tt.wantErr)
-// 		})
-// 	}
-// }
+			assert.Equal(t, (err != nil), tt.wantErr)
+		})
+	}
+}
 
-// func TestService_GetMetric(t *testing.T) {
-// 	type metric struct {
-// 		ID         string
-// 		MetricType string
-// 		Hash       string
-// 		Delta      int64
-// 		Value      float64
-// 	}
-// 	tests := []struct {
-// 		name    string
-// 		metric  metric
-// 		wantErr bool
-// 	}{
-// 		{
-// 			name:    "pisitive gauge test",
-// 			metric:  metric{ID: "TotalAlloc", MetricType: "gauge", Value: 175368, Hash: "ecd17becc5c0cb83489bbd3387aa351ea590ce64b44ac4b06645f058f36d20c2"},
-// 			wantErr: false,
-// 		},
-// 		{
-// 			name:    "pisitive counter test",
-// 			metric:  metric{ID: "PollCount", MetricType: "counter", Delta: 5, Hash: "b54c435bba4ef9334d8b0ca2938c912a75660e5a152a0fb68a4177dfccdaf9e9"},
-// 			wantErr: false,
-// 		},
-// 	}
+func TestService_GetMetric(t *testing.T) {
+	type metric struct {
+		ID         string
+		MetricType string
+		Hash       string
+		Delta      int64
+		Value      float64
+	}
+	tests := []struct {
+		name    string
+		metric  metric
+		wantErr bool
+	}{
+		{
+			name:    "pisitive gauge test",
+			metric:  metric{ID: "TotalAlloc", MetricType: "gauge", Value: 175368, Hash: "ecd17becc5c0cb83489bbd3387aa351ea590ce64b44ac4b06645f058f36d20c2"},
+			wantErr: false,
+		},
+		{
+			name:    "pisitive counter test",
+			metric:  metric{ID: "PollCount", MetricType: "counter", Delta: 5, Hash: "b54c435bba4ef9334d8b0ca2938c912a75660e5a152a0fb68a4177dfccdaf9e9"},
+			wantErr: false,
+		},
+	}
 
-// 	cfg := models.Config{Key: "qwerty"}
-// 	r := storage.NewStorage(&cfg, nil)
-// 	s := *NewService(&cfg, r)
+	cfg := models.Config{Key: "qwerty"}
+	r := storage.NewStorage(&cfg, nil)
+	s := *NewService(&cfg, r)
 
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
 
-// 			newMetric := models.Metric{
-// 				ID:         tt.metric.ID,
-// 				MetricType: tt.metric.MetricType,
-// 				Hash:       &tt.metric.Hash,
-// 			}
+			newMetric := models.Metric{
+				ID:         tt.metric.ID,
+				MetricType: tt.metric.MetricType,
+				Hash:       &tt.metric.Hash,
+			}
 
-// 			if newMetric.MetricType == "counter" {
-// 				newMetric.Delta = &tt.metric.Delta
-// 			} else {
-// 				newMetric.Value = &tt.metric.Value
-// 			}
+			if newMetric.MetricType == "counter" {
+				newMetric.Delta = &tt.metric.Delta
+			} else {
+				newMetric.Value = &tt.metric.Value
+			}
 
-// 			err := s.SetMetric(newMetric)
-// 			assert.NoError(t, err)
-// 			err = s.GetMetric(&newMetric)
-// 			assert.NoError(t, err)
+			err := s.SetMetric(newMetric)
+			assert.NoError(t, err)
+			err = s.GetMetric(&newMetric)
+			assert.NoError(t, err)
 
-// 			newHash, err := hex.DecodeString(*newMetric.Hash)
-// 			assert.NoError(t, err)
+			newHash, err := hex.DecodeString(*newMetric.Hash)
+			assert.NoError(t, err)
 
-// 			hash, err := hex.DecodeString(tt.metric.Hash)
-// 			assert.NoError(t, err)
+			hash, err := hex.DecodeString(tt.metric.Hash)
+			assert.NoError(t, err)
 
-// 			assert.Equal(t, !hmac.Equal(newHash, hash), tt.wantErr)
-// 		})
-// 	}
-// }
+			assert.Equal(t, !hmac.Equal(newHash, hash), tt.wantErr)
+		})
+	}
+}
